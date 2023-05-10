05e055fa298edaaa503094fbfae19d1ef607b8cd
jkray <jkray29@gmail.com>
2023-05-10 21:28:50 +0800

    0510 JS基礎

---
+++ dist/js/js_base.js
@@ -0,0 +1,11 @@
+
+let age = 18;
+let firstName = 'David';
+let lastName = 'Lin';
+let country = 'TW';
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+
+console.log(`(${country}) ${firstName} ${lastName}`);
+
+console.log(age);
