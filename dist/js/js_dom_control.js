e3565ccc6adb5d3aa39ed78ae52c75cbfd2269d4
jkray <jkray29@gmail.com>
2023-05-22 21:27:47 +0800

    0522手機選單icon切換

---
+++ dist/js/js_dom_control.js
@@ -0,0 +1,32 @@
+let uid = document.querySelector('#uid');
+
+let doing = document.querySelector('#doing');
+
+doing.addEventListener('click', () => {
+    uid.style.color = 'red';
+    uid.style.fontSize = '50px';
+    uid.style.backgroundColor = 'yellow';
+});
+
+let addClass = document.querySelector('#add-class');
+
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+});
+
+let removeClass = document.querySelector('#remove-class');
+
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+});
+
+let mobileIcon = document.querySelector('#mobile-icon');
+
+
+mobileIcon.addEventListener('click', () => {
+    if (mobileIcon.classList.contains('active')) {
+        mobileIcon.classList.remove('active');
+    } else {
+        mobileIcon.classList.add('active');
+    }
+});
