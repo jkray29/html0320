37fdb61ad4c76089b898b14abe0bed4adfc184c5
jkray <jkray29@gmail.com>
2023-06-05 21:29:41 +0800

    0605待辦清單未完

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -1,3 +1,20 @@
+const database = {
+    set(key, value) {
+        value = JSON.stringify(value);
+        localStorage.setItem(key, value);
+    },
+    get(key, def) {
+        let value = localStorage.getItem(key);
+        if (value) {
+            value = JSON.parse(value);
+            return value;
+        }
+        return def;
+    },
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+}
 let vm = Vue.createApp({
     data() {
         return {
@@ -24,16 +41,30 @@ let vm = Vue.createApp({
             this.pending.push(value);
             this.itemValue = '';
             this.$refs.itemValue.focus();
+            this.update();
+        },
+        doRemove(index) {
+            console.log(index);
         },
         toDone(index) {
             let value = this.pending[index];
             this.done.push(value);
             this.pending.splice(index, 1);
+            this.update();
         },
         toPending(index) {
             let value = this.done[index];
             this.pending.push(value);
             this.done.splice(index, 1);
+            this.update();
+        },
+        update() {
+            database.set('todo-pending', this.pending);
+            database.set('todo-done', this.done);
         }
+    },
+    mounted() {
+        this.pending = database.get('todo-pending', []);
+        this.done = database.get('todo-done', []);
     }
 }).mount('#app');
