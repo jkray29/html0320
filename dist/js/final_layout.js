f3edb46bff12216e0819e8ffa10be2ce13c347b3
jkray <jkray29@gmail.com>
2023-06-19 21:30:24 +0800

    0619切換圖片

--- dist/js/final_layout.js
+++ dist/js/final_layout.js
@@ -3,16 +3,30 @@ Vue.createApp({
         return {
             items: [
                 { title: '第一張', subtitle: '第一子標題', bg: '/image/dog.jpeg' },
-                { title: '第二張', subtitle: '第二子標題', bg: '/image/dog.jpeg' },
-                { title: '第三張', subtitle: '第三子標題', bg: '/image/dog.jpeg' },
+                { title: '第二張', subtitle: '第二子標題', bg: '/image/banner.jpg' },
+                { title: '第三張', subtitle: '第三子標題', bg: '/image/banner1.jpg' },
 
             ],
             currentIndex: 0,
+            timer: '',
+            loopSecond: 3
         }
     },
     methods: {
         setIndex(index) {
             this.currentIndex = index;
+            this.doLoop();
+        },
+        doLoop() {
+            clearInterval(this.timer);
+            this.timer = setInterval(() => {
+                let max = this.items.length - 1;
+                if (this.currentIndex >= max) {
+                    this.currentIndex = 0;
+                } else {
+                    this.currentIndex++;
+                }
+            }, this.loopSecond * 1000);
         }
     }
 }).mount('#app-slider');
