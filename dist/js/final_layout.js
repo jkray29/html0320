5192cec42160fe8d1d0b507670bc3e0a2c6747a4
jkray <jkray29@gmail.com>
2023-06-19 21:21:10 +0800

    0919

--- dist/js/final_layout.js
+++ dist/js/final_layout.js
@@ -1,1 +1,18 @@
-alert(1);
+Vue.createApp({
+    data() {
+        return {
+            items: [
+                { title: '第一張', subtitle: '第一子標題', bg: '/image/dog.jpeg' },
+                { title: '第二張', subtitle: '第二子標題', bg: '/image/dog.jpeg' },
+                { title: '第三張', subtitle: '第三子標題', bg: '/image/dog.jpeg' },
+
+            ],
+            currentIndex: 0,
+        }
+    },
+    methods: {
+        setIndex(index) {
+            this.currentIndex = index;
+        }
+    }
+}).mount('#app-slider');
